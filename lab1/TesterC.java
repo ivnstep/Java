882cdc3dc298df015559906f34900d263eefcae1
ivnstep <113294168+ivnstep@users.noreply.github.com>
2022-10-31 17:35:05 +0300

    Add files via upload

--- lab1/TesterC.java
+++ lab1/TesterC.java
@@ -7,13 +7,14 @@ public class TesterC {
 
     public TesterC() {
         Random r = new Random();
-        l = r.nextInt(100);
+        l = r.nextInt(99) + 1;
         ar = new Circle[l];
         for (int i = 0; i < ar.length; ++i) {
             Point p = new Point();
             p.setXY(r.nextInt(), r.nextInt());
+            ar[i] = new Circle();
             ar[i].setC(p);
-            ar[i].setR(r.nextInt(1000));
+            ar[i].setR(r.nextInt(1000) + 1);
         }
     }
 
@@ -30,6 +31,43 @@ public class TesterC {
     public void setAr(Circle[] ar) {
         this.ar = ar;
         this.l = ar.length;
+    }
+
+    public Circle min () {
+        int min = 0;
+        double c = ar[0].getR();
+        for (int i = 1; i < l; ++i) {
+            if (c < ar[i].getR()) {
+                min = i;
+                c = ar[i].getR();
+            }
+        }
+        return ar[min];
+    }
+
+    public Circle max () {
+        int max = 0;
+        double c = ar[0].getR();
+        for (int i = 1; i < l; ++i) {
+            if (c > ar[i].getR()) {
+                max = i;
+                c = ar[i].getR();
+            }
+        }
+        return ar[max];
+    }
+
+    public void sort () {
+        Circle c = new Circle();
+        for (int i = 0; i < l; ++i) {
+            for (int j = 1; j < l; ++j) {
+                if (ar[j].getR() < ar[j-1].getR()) {
+                    c = ar[j];
+                    ar[j] = ar[j - 1];
+                    ar[j - 1] = c;
+                }
+            }
+        }
     }
 
     public Circle[] getAr() {
